a270bd8ccb5e8e7fd5c7a0ecaae420cfee91b11b
sukesh <sukesh.iist@gmail.com>
2019-12-15 19:03:09 +0530

    first commit

---
+++ app/src/main/java/io/swasthya/timer/MainActivity.java
@@ -0,0 +1,112 @@
+package io.swasthya.timer;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TimePicker;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageUpload;
+    private EditText name, address;
+    private Button addTimerButton;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    String TAG = "RemindMe";
+    LocalData localData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        createNotificationChannel();
+        localData = new LocalData(getApplicationContext());
+
+        imageUpload = findViewById(R.id.imageUpload);
+        name = findViewById(R.id.name);
+        address = findViewById(R.id.address);
+        addTimerButton = findViewById(R.id.addTimer);
+        imageUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+        });
+        addTimerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //if (localData.getReminderStatus())
+                    showTimePickerDialog(localData.get_hour(), localData.get_min());
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageUpload.setImageBitmap(imageBitmap);
+        }
+    }
+
+    private void showTimePickerDialog(int h, int m) {
+
+        LayoutInflater inflater = getLayoutInflater();
+        View view = inflater.inflate(R.layout.timepicker_header, null);
+
+        TimePickerDialog builder = new TimePickerDialog(this, R.style.DialogTheme,
+                new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hour, int min) {
+                        Log.d(TAG, "onTimeSet: hour " + hour);
+                        Log.d(TAG, "onTimeSet: min " + min);
+                        localData.set_hour(hour);
+                        localData.set_min(min);
+                        //tvTime.setText(getFormatedTime(hour, min));
+                        NotificationScheduler.setReminder(MainActivity.this, AlarmReceiver.class, localData.get_hour(), localData.get_min());
+
+
+                    }
+                }, h, m, false);
+
+        builder.setCustomTitle(view);
+        builder.show();
+
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(NotificationScheduler.CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+}
